2d478f4942b6eb2cac0a12f7f4d078167a495cd3
Régis Teixeira <regismarcondes2016@gmail.com>
2022-06-22 21:06:20 -0300

    corrigindo erros

---
+++ app/src/main/java/com/example/words2/DetailActivity.kt
@@ -0,0 +1,36 @@
+package com.example.words2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.words2.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    companion object {
+        const val LETTER = "letter"
+        const val SEARCH_PREFIX = "https://www.google.com/search?q="
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val letterId = intent?.extras?.getString("letter").toString()
+
+
+
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = WordAdapter(letterId, this)
+
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        )
+
+        title = getString(R.string.detail_prefix) + "" + letterId
+    }
+}
